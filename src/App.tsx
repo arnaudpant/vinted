596265036a0b9ef8929f42f5889cfa1526d1a2b5
macbook kal <arnaud.pantenier@gmail.com>
2023-12-24 11:42:06 +0100

    ajout Button pour tester shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button"
+
 function App() {
   return (
     <>
       <h1 className="text-3xl">Vinted</h1>
+      <Button>Click me</Button>
     </>
   );
 }
