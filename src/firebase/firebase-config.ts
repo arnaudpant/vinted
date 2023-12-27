19288cd8cf9d1dba5f8829384df6077bc0745fc1
macbook kal <arnaud.pantenier@gmail.com>
2023-12-27 16:36:26 +0100

    ajout firebase.config

---
+++ src/firebase/firebase-config.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBemG2dk7iQ6jtmVgTU81sy5-H_HqvMML4',
+  authDomain: 'bootcamp-project-vinted.firebaseapp.com',
+  projectId: 'bootcamp-project-vinted',
+  storageBucket: 'bootcamp-project-vinted.appspot.com',
+  messagingSenderId: '239313519946',
+  appId: '1:239313519946:web:fbcb5d48926508d982adb3',
+};
+
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
