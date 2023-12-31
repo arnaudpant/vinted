0dda7377aa8a9abd0a0a6659f796bfce07269434
ghorreso <omid.ghorreshi@gmail.com>
2023-12-31 16:16:56 +0100

    feat: footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,53 @@
+import FooterPrivacySection from './footer/FooterPrivacySection';
+import FooterSocialSectionApp from './footer/FooterSocialSectionApp';
+import FooterSocialSectionMedia from './footer/FooterSocialSectionMedia';
+import FooterSectionItem from './footer/FooterSectionItem';
+
+const Footer = () => {
+  return (
+    <footer className="bg-stone-100">
+      <div className="container mt-auto divide-y">
+        <ul className="flex flex-col justify-between sm:flex-row">
+          <FooterSectionItem
+            label="Vinted"
+            linkLabels={[
+              'A propos de Vinted',
+              'Carrière',
+              'Le développement durable',
+              'Presse',
+              'Publicités',
+            ]}
+          />
+          <FooterSectionItem
+            label="Découvrir"
+            linkLabels={[
+              'Comment ça marche ?',
+              "Vérification de l'article",
+              'Applications mobiles',
+              'Tableau de bord',
+              'Vinted Pro',
+              'Guide Vinted Pro',
+            ]}
+          />
+          <FooterSectionItem
+            label="Aide"
+            linkLabels={[
+              "Centre d'aide",
+              'Vendre',
+              'Acheter',
+              'Confiance et sécurité',
+            ]}
+          />
+          <FooterSectionItem label="Communauté" linkLabels={['Forum']} />
+        </ul>
+        <div className="flex flex-col-reverse items-center justify-between p-3 sm:flex-row">
+          <FooterSocialSectionMedia />
+          <FooterSocialSectionApp />
+        </div>
+        <FooterPrivacySection />
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,11 @@
-
+import Footer from '@/components/Footer';
 
 const Home = () => {
-    return (
-        <div className="bg-vintedBackground">
-
-        </div>
-    );
+  return (
+    <div className="bg-vintedBackground">
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
