3d6f0752e863b47081c0c079a20e825793945bf7
Baptiste GRIMALDI <100611587+BaptisteGrimaldi@users.noreply.github.com>
2024-01-05 17:36:27 +0100

    Refactor ProductCard component and fetch products from API

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,41 +1,47 @@
-import { useEffect, useRef, useState } from 'react';
+import ProductCard from '@/components/home/ProductCard';
+
+import { useEffect, useState } from 'react';
 
 const Home = () => {
-  const [products, setProducts] = useState<Product[]>([]);
+  const [productsCreateur, setProductsCreateur] = useState<Product[]>([]);
+  const [productsPopulaires, setProductsPopulaires] = useState<Product[]>([]);
 
   useEffect(() => {
-    async function fetchProducts(): Promise<void> {
+    async function fetchProductsCreateur(): Promise<void> {
       try {
         const response = await fetch(
           'https://fakestoreapi.com/products?limit=5',
         );
         const productsData: Product[] = await response.json();
-        setProducts(productsData);
+        setProductsCreateur(productsData);
       } catch (error) {
         console.error('Erreur lors de la récupération des données :', error);
       }
     }
 
-    fetchProducts();
-  }, []);
-
-  const [productListHeight, setProductListHeight] = useState(0);
-  const productListRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const updateProductListHeight = () => {
-      if (productListRef.current) {
-        setProductListHeight(productListRef.current.clientHeight);
+    async function fetchProductsPopulaires(): Promise<void> {
+      try {
+        const response = await fetch(
+          'https://fakestoreapi.com/products',
+        )
+        .then((res)=>{
+          return res.json()
+        })
+        .then((data:Product[])=>{
+          return data.slice(-5)
+        });
+        
+        setProductsPopulaires(response);
+      } catch (error) {
+        console.error('Erreur lors de la récupération des données :', error);
       }
-    };
+    }
 
-    updateProductListHeight();
-    window.addEventListener('resize', updateProductListHeight);
-
-    return () => {
-      window.removeEventListener('resize', updateProductListHeight);
-    };
+    fetchProductsPopulaires();
+    fetchProductsCreateur();
   }, []);
+
+
 
   const marques: string[] = [
     'Tommy Hilfiger',
@@ -61,60 +67,26 @@ const Home = () => {
     'Versace',
     'Burberry',
     'Valentino',
-    'Givenchy',
-    'Yves Saint Laurent',
-    'Chloé',
-    'Kenzo',
-    'Sandro',
-    'Diesel',
-    'Guess',
-    'The Kooples',
-    'Ralph Lauren',
-    'Calvin Klein',
-    'Armani',
-    'Sonia Rykiel',
-    'Sergio Rossi',
-    'Sandro',
-    'Sézan',
   ];
 
   return (
     <div className="w-7/12 m-auto">
-      <div className="flex justify-between w-full">
-        <h1 className="h1">Explorer les articles de créateur</h1>
-        <p className="toutVoir">Tout voir</p>
-      </div>
-      <div className="flex space-x-10 mt-16" ref={productListRef}>
-        {products.map((product: Product) => (
-          <div
-            key={product.id}
-            className="flex flex-col items-center w-[200px]"
-          >
-            <img
-              src={product.image}
-              alt={product.title}
-              className="w-[200px] h-[200px]"
-            />
-            <p>{product.price} €</p>
-            <h2 className="text-center">{product.title}</h2>
-          </div>
-        ))}
-        <div className="flex-col flex  items-center w-[200px] bg-gray-100">
-          <p
-            className="text-vintedTextGrisFonce"
-            style={{ marginTop: `${productListHeight / 2 - 10}px` }}
-          >
-            Voir tous les articles
-          </p>
-        </div>
-      </div>
-      {/* {Le meme composant dès que l'api fonctionne de nouveau} */}
 
-      <div>
+      <div className='mt-20'>
+        <ProductCard title="Explorer les articles de créateur" products={productsCreateur} />
+      </div>
+
+      <div className='mt-20'>
+        <ProductCard title="Articles populaires" products={productsPopulaires} />
+      </div>
+
+      <div className='mt-20 mb-20'>
         <h1 className="h1">Rechercher par marque</h1>
         <div className="flex flex-wrap gap-x-3">
-          {marques.map((marque, index) => (
-            <div key={index} className='border p-2 mt-3'>{marque}</div>
+          {marques.map((marque) => (
+            <div key={marque} className="border p-2 mt-3">
+              {marque}
+            </div>
           ))}
         </div>
       </div>
