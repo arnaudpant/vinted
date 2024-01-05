a1fbc0db2f7a5349219bc572f18603edcdb8541f
macbook kal <arnaud.pantenier@gmail.com>
2024-01-05 20:32:54 +0100

    ajout BTN left right + taille container

--- src/components/suggestion/SuggestionSearch.tsx
+++ src/components/suggestion/SuggestionSearch.tsx
@@ -1,8 +1,13 @@
 import { ListSuggestSearchFromVinted } from '../../api/api';
 import { useEffect, useState } from 'react';
 import CardSuggestionSearch from './CardSuggestionSearch';
+import { ChevronRight, ChevronLeft } from 'lucide-react';
 
 const SuggestionSearch = () => {
+  const [scrollCards, setScrollCard] = useState<'left' | 'right'>('right');
+  const divToScroll = document.getElementById('showScroll');
+
+
   const [products, setProducts] = useState<
     typeof ListSuggestSearchFromVinted | []
   >([]);
@@ -12,10 +17,24 @@ const SuggestionSearch = () => {
   }, []);
 
 
+  const handleClicRight = () => {
+    divToScroll?.scrollTo({
+      left: 1400,
+      behavior: 'smooth',
+    });
+    setScrollCard('left');
+  };
+  const handleClicLeft = () => {
+    divToScroll?.scrollTo({
+      left: 0,
+      behavior: 'smooth',
+    });
+    setScrollCard('right');
+  };
 
   return (
     //TODO: Supprimer container a l'integration
-    <div className="container mx-auto py-12">
+    <div className="container mx-auto py-12 max-w-[1240px]">
       <h2 className="text-2xl pb-4">Suggestions de recherche</h2>
       <div className="relative h-[75px] overflow-hidden">
         <div
@@ -27,6 +46,22 @@ const SuggestionSearch = () => {
               <CardSuggestionSearch key={product.marque} product={product} />
             ))}
           </div>
+          {scrollCards === 'right' && (
+            <div
+              className="absolute top-5 right-2 flex items-center justify-center h-8 w-8 rounded-full bg-gray-600 opacity-40 cursor-pointer"
+              onClick={handleClicRight}
+            >
+              <ChevronRight className="text-vintedBackgrounf" />
+            </div>
+          )}
+          {scrollCards === 'left' && (
+            <div
+              className="absolute top-5 left-2 flex items-center justify-center h-8 w-8 rounded-full bg-gray-600 opacity-40 cursor-pointer"
+              onClick={handleClicLeft}
+            >
+              <ChevronLeft className="text-vintedBackgrounf" />
+            </div>
+          )}
         </div>
       </div>
     </div>
