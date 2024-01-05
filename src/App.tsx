3b2e03113a6928cb7a2507a1639629c5c653bd67
macbook kal <arnaud.pantenier@gmail.com>
2024-01-05 14:13:38 +0100

    Ajout env.exemple

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import Catalog from './pages/Catalog';
 
 function App() {
   return (
-    <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -25,7 +24,6 @@ function App() {
           <Route path="/*" element={<Error404 />} />
         </Routes>
       </BrowserRouter>
-    </>
   );
 }
 
