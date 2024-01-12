fdd69e9536f2ab4a27670fc309ba121f3d83e742
macbook kal <arnaud.pantenier@gmail.com>
2024-01-12 18:44:42 +0100

    Fin tests unitaires

--- __tests__/BoxInfosVendeur.test.tsx
+++ __tests__/BoxInfosVendeur.test.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { describe, expect, test } from 'vitest';
-import BoxInfosVendeur from './../src/components/box-infos-vendeur/BoxInfosVendeur'
-import { render, screen } from '@testing-library/react';
+import { afterEach, describe, expect, test } from 'vitest';
+import BoxInfosVendeur from './../src/components/box-infos-vendeur/BoxInfosVendeur';
+import { cleanup, fireEvent, render, screen } from '@testing-library/react';
 
-describe('Initialisation du component BoxInfosVendeur', ()=> {
-    test('Affichage infos reduit', ()=> {
-        render(<BoxInfosVendeur />)
-        expect(screen.getByText(/... Voir plus/i)).toBeInTheDocument();
-    })
-})
+describe('Initialisation du component BoxInfosVendeur', () => {
+  afterEach(() => {
+    cleanup();
+  });
+  test('Affichage infos reduit', () => {
+    render(<BoxInfosVendeur />);
+    expect(screen.getByText(/... Voir plus/i)).toBeInTheDocument();
+    expect(
+      screen.queryByText(/droit des obligations/i),
+    ).not.toBeInTheDocument();
+  });
+  test('Clic sur Voir plus', () => {
+    render(<BoxInfosVendeur />);
+    const seeMore = screen.getByText(/... Voir plus/i);
+    fireEvent.click(seeMore);
+    expect(screen.queryByText(/... Voir plus/i)).not.toBeInTheDocument();
+    expect(screen.getByText(/responsabilité civile/i)).toBeInTheDocument();
+  });
+});
