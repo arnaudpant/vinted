8a3c226bd5aa25324574d51b888699606cdfbef8
macbook kal <arnaud.pantenier@gmail.com>
2024-01-13 15:35:19 +0100

    COR App

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,9 @@
 import Footer from '@/components/Footer';
 import BoxInfosVendeur from '@/components/box-infos-vendeur/BoxInfosVendeur';
-import SuggestionSearch from '@/components/suggestion/SuggestionSearch';
 
 const Home = () => {
   return (
     <div className="bg-vintedBackground">
-      <SuggestionSearch />
       <BoxInfosVendeur />
       <Footer />
     </div>
