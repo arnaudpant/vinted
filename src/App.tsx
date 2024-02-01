acdcc686320038b84e41a3149fa8952c6749b5d7
macbook kal <arnaud.pantenier@gmail.com>
2024-02-01 22:43:21 +0100

    COR erreur context

--- src/App.tsx
+++ src/App.tsx
@@ -12,32 +12,29 @@ import NewArticle from './pages/NewArticle';
 import Search from './pages/Search';
 import User from './pages/User';
 import Authentification from './pages/Authentification';
-import { FakeShopProvider } from './context/FakeShopContext';
 
 function App() {
   return (
-    <FakeShopProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Home />} />
-            {/* Page avec modal connexion ou inscruption */}
-            <Route path="/authentification" element={<Authentification />} />
-            {/*Page de profil de l'utilisateur*/}
-            <Route path="/member/:userId" element={<User />} />
-            {/* Page Achat */}
-            <Route path="/items/:id" element={<Article />} />
-            {/* Page vente si connecté */}
-            <Route path="/new-article" element={<NewArticle />} />
-            {/* Page de recherche global */}
-            <Route path="/search" element={<Search />} />
-            {/* Page de recherche par categorie via navbar */}
-            <Route path="/catalog/:categoryId" element={<Catalog />} />
-            <Route path="/*" element={<Error404 />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </FakeShopProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          {/* Page avec modal connexion ou inscruption */}
+          <Route path="/authentification" element={<Authentification />} />
+          {/*Page de profil de l'utilisateur*/}
+          <Route path="/member/:userId" element={<User />} />
+          {/* Page Achat */}
+          <Route path="/items/:id" element={<Article />} />
+          {/* Page vente si connecté */}
+          <Route path="/new-article" element={<NewArticle />} />
+          {/* Page de recherche global */}
+          <Route path="/search" element={<Search />} />
+          {/* Page de recherche par categorie via navbar */}
+          <Route path="/catalog/:categoryId" element={<Catalog />} />
+          <Route path="/*" element={<Error404 />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
