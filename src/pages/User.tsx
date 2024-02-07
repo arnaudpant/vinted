a08289ddaeb9bd2a742d59b94b29bc73aa29e937
Nabil Benguernane <nabil.benguernane@gmail.com>
2024-02-07 17:24:00 +0100

    fix41 - ModalAuth qui apparait furtivement pendant le chargement des infos user

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,18 +1,39 @@
-import ModalAuth from '@/components/modals/authentification/ModalAuth';
+import LoadingCircularProgress from '@/components/ui/LoadingCircularProgress';
 import UserProfil from '@/components/userProfil/UserProfil';
 import useFirebaseAuth from '@/hooks/useFirebaseAuth';
+import { useParams } from 'react-router-dom';
 import UserContext from '../context/UserContext';
 
 const User = () => {
-  const { authUser: user } = useFirebaseAuth();
+  const { authUser: user, authUserIsLoading } = useFirebaseAuth();
 
-  if (!user) return <ModalAuth setModalConnexion={() => {}} />;
+  const { userId } = useParams();
 
-  return (
-    <UserContext.Provider value={user}>
-      <UserProfil />;
-    </UserContext.Provider>
-  );
+  const isUserIdFromConnectedUser = userId === user?.uid;
+
+  if (!user && authUserIsLoading) return <LoadingCircularProgress />;
+
+  if (!user && !authUserIsLoading && !isUserIdFromConnectedUser)
+    return (
+      <p className="mb-4 text-center text-red-500">
+        Veuillez vous authentifier
+      </p>
+    );
+
+  if (user && !authUserIsLoading && !isUserIdFromConnectedUser) {
+    return (
+      <p className="mb-4 text-center text-red-500">
+        Vous n&apos;avez pas accès à ce compte
+      </p>
+    );
+  }
+
+  if (user && !authUserIsLoading && isUserIdFromConnectedUser)
+    return (
+      <UserContext.Provider value={user}>
+        <UserProfil />;
+      </UserContext.Provider>
+    );
 };
 
 export default User;
