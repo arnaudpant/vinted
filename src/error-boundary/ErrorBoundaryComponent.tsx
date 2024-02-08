3317a4c5455b5c1f8d3ba5e2ca364d4f795ca0ce
Nabil Benguernane <nabil.benguernane@gmail.com>
2024-02-08 16:30:16 +0100

    fix correction de l'affichage de ErrorBoundaryComponent

--- src/error-boundary/ErrorBoundaryComponent.tsx
+++ src/error-boundary/ErrorBoundaryComponent.tsx
@@ -9,14 +9,9 @@ const ErrorBoundaryComponent: React.ComponentType<FallbackProps> = ({
     <div className=" relative flex h-[100vh] flex-col items-center justify-center bg-cover bg-center">
       <div className="flex flex-col items-center justify-center gap-5">
         <h1 className="text-4xl font-bold ">Oups une erreur est survenue !</h1>
-        <p className="text-sm text-white">
-          Désolé, nous n&apos;avons pas trouvé cette page.
-        </p>
-        <div>
-          <Link to="/" onClick={resetErrorBoundary}>
-            Retourner à l&apos;accueil
-          </Link>
-        </div>
+        <Link to="/" onClick={resetErrorBoundary}>
+          Retourner à l&apos;accueil
+        </Link>
       </div>
       <div className="absolute bottom-8">
         <p className="">Code d&apos;erreur : {error.message}</p>
